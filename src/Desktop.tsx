8270f1d00c5851a904207fc3766374964906bb92
choidabom <dabomi0305@naver.com>
2024-11-14 16:15:36 +0900

    add: aria-label to improve accessibility since button uses an emoji

--- src/Desktop.tsx
+++ src/Desktop.tsx
@@ -7,6 +7,7 @@ const Desktop = (): JSX.Element => {
         <div className="bg-light dark:bg-dark bg-cover w-screen h-screen">
             <button
                 className="absolute right-5 top-5 p-2 rounded-full bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700"
+                aria-label={`Switch to ${desktopMode === 'light' ? 'dark' : 'light'} mode`}
                 onClick={toggleDesktopMode}
             >
                 {desktopMode === 'light' ? '🌞' : '🌙'}
