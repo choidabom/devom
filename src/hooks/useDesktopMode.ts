b7a6ff07466d53fe62c666b85a2fca9051e55120
choidabom <dabomi0305@naver.com>
2024-11-14 16:11:08 +0900

    add: save mode in localStorage and check user prefer system setting (window.matchMedia)

--- src/hooks/useDesktopMode.ts
+++ src/hooks/useDesktopMode.ts
@@ -1,22 +1,35 @@
 import { useEffect, useState } from 'react';
 
+type DesktopMode = 'light' | 'dark';
+
 export const useDesktopMode = (): {
     desktopMode: string;
     toggleDesktopMode: () => void;
 } => {
-    const [desktopMode, setDesktopMode] = useState<'light' | 'dark'>('light');
+    const [desktopMode, setDesktopMode] = useState<DesktopMode>('light');
 
     const toggleDesktopMode = (): void => {
-        setDesktopMode(desktopMode === 'light' ? 'dark' : 'light');
+        setDesktopMode((currentMode) => {
+            const newMode: DesktopMode = currentMode === 'light' ? 'dark' : 'light';
+            localStorage.setItem('theme', newMode);
+            document.documentElement.classList.replace(currentMode, newMode);
+
+            return newMode;
+        });
     };
 
     useEffect(() => {
-        document.documentElement.classList.toggle('dark', desktopMode === 'dark');
+        const theme = localStorage.getItem('theme') as DesktopMode;
+        const preferDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+        const mode = theme || (preferDark ? 'dark' : 'light');
+        document.documentElement.classList.add(mode);
+        setDesktopMode(mode);
 
         return (): void => {
-            document.documentElement.classList.remove('dark');
+            document.documentElement.classList.remove('light', 'dark');
         };
-    }, [desktopMode]);
+    }, []);
 
     return { desktopMode, toggleDesktopMode };
 };
