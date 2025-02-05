976f40dacefabe492f06c90f1f966a3015a01f7e
choidabom <dabomi0305@naver.com>
2025-02-06 00:57:17 +0900

    add(todolist): install tailwindcss and add NotoSans font

--- apps/todolist/src/main.tsx
+++ apps/todolist/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./TodoList.tsx";
+import TodoList from "./TodoList.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TodoList />
   </StrictMode>
 );
