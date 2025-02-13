a8f243b5a12a60d06daf71b751818a0601f66784
choidabom <dabomi0305@naver.com>
2025-02-13 23:05:13 +0900

    fix: set conditional config in vite.config.ts

--- apps/archive/vite.config.ts
+++ apps/archive/vite.config.ts
@@ -3,7 +3,9 @@ import type { PluginOption } from 'vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
+// https://ko.vite.dev/config/#conditional-config
+
 export default defineConfig({
     plugins: [react()] as PluginOption[],
-    base: process.env.NODE_DEV === 'production' ? '/devom' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/devom' : '/',
 });
