36756091fa688d91a622f3e551c867b8eaf3b62f
choidabom <dabomi0305@naver.com>
2025-02-25 12:45:40 +0900

    rename(ts-config): tsconfig to ts-config

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       fileName: "index",
     },
     rollupOptions: {
-      external: ["@devom/tsconfig"],
+      external: ["@devom/ts-config"],
     },
   },
 });
