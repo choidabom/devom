26ac30306f8f86d474b950fffc61fc6e12d74bb6
choidabom <dabomi0305@naver.com>
2025-03-06 18:49:12 +0900

    fix(todolist): remove plugin and add `intro` for including css in dist/index.js

--- apps/todolist/vite.config.ts
+++ apps/todolist/vite.config.ts
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { type PluginOption, defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import libCss from "vite-plugin-libcss";
 
 const ABSOLUTE_PATH_REGEX = /^(?:\/|(?:[A-Za-z]:)?[/\\|])/;
 const RELATIVE_PATH_REGEX = /^\.?\.(\/|$)/;
@@ -18,7 +17,6 @@ export default defineConfig({
       include: ["src"],
       rollupTypes: true,
     }),
-    libCss(),
   ] as PluginOption[],
   build: {
     lib: {
@@ -26,9 +24,16 @@ export default defineConfig({
       fileName: "index",
       formats: ["es"],
     },
+    cssCodeSplit: false,
     rollupOptions: {
       external: (id: string): boolean => !isAbsolute(id) && !isRelative(id),
       output: {
+        intro: (chunk) => {
+          if (chunk.fileName === "index.js") {
+            return `import "./style.css";`;
+          }
+          return "";
+        },
         entryFileNames: "index.js",
         chunkFileNames: "chunk-[name].js",
         assetFileNames: "[name].[ext]",
