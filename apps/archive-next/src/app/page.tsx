8e6e4e1b551b92a1801f5299cd52c33a1404ca1f
choidabom <dabomi0305@naver.com>
2025-10-20 21:09:46 +0900

    fix: mdx config in next.config.ts

--- apps/archive-next/src/app/page.tsx
+++ apps/archive-next/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation"
 export default function Home() {
-  return <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">archive</div>
+  return redirect("/blog")
 }
